1c11ab63c5482a6a2bcf6357a87d57d6fa0d8244
luanavalerio1 <luanavallerio@gmail.com>
2022-09-17 16:08:12 -0300

    figma

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { FcReuse } from "react-icons/fc";
 import * as S from "./styles";
 import { Link } from "react-router-dom";
+import imagem from "assets/img/image9.jpg"
 
 const Menu = () => {
   return (
     <S.Cabecalho>
       <picture>
         <Link to="/">
-          <FcReuse />
+         <img src={imagem} alt="Imagens principais" />
         </Link>
       </picture>
       <nav>
--- src/services/dados.ts
+++ src/services/dados.ts
@@ -1,17 +1,16 @@
+import { Script } from "vm"
+
 const dados = [
   {
     id: 1,
-    nome: O Sistema Solar é o nosso endereço cósmico. 
-     Está localizado no braço de Órion,
-     um dos braços da Via Láctea,
-      é composto por oito planetas ao todo
+    nome: "O Sistema Solar é o nosso endereço cósmico. Está localizado no braço de Órion, um dos braços da Via Láctea, é composto por oito planetas ao todo. "
+    
+    
   },
   {
     id: 2,
-    nome: Os buracos negros podem ter diversos tamanhos. 
-    Alguns cientistas afirmam que existem buracos negros do tamanho de átomos que são tão massivos quanto uma montanha. 
-    Também existem buracos negros da escala de estrelas. 
-    Suas massas são mais de 20 vezes maiores que a massa do nosso Sol.",
+    nome: "Os buracos negros podem ter diversos tamanhos. Alguns cientistas afirmam que existem buracos negros do tamanho de átomos que são tão massivos quanto uma montanha. Também existem buracos negros da escala de estrelas. Suas massas são mais de 20 vezes maiores que a massa do nosso Sol."
+    
   },
 ]
 
